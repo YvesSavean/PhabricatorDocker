d88519326145bd4f11c5f2f2ca783b17736be62b
Yves Savean <savean.yves@gmail.com>
2019-08-04 18:30:20 +0200

    version 1.0.0-SNAPSHOT fonctionnel

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ envsubst < "/tmp/httpd.template" > "${BASEDIR_APACHE}/conf/httpd.conf"
 envsubst < "/tmp/local.template" >  "${APACHE_ROOT_DIR}/phabricator/conf/local/local.json"
 
 #On met a jour la database si besoin
-#cd ${APACHE_ROOT_DIR}/phabricator && ./bin/storage upgrade --force
+cd ${APACHE_ROOT_DIR}/phabricator && ./bin/storage upgrade --force
 
 #on demarrage apache
 /usr/sbin/httpd -D FOREGROUND
